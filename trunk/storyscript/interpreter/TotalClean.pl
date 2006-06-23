4bc7c4b551f84f4e20947c23b755b4ce54f30081
danielcj <danielcj@dc183c02-a200-0410-8664-8f6ecc32c94b>
2006-06-23 03:21:30 +0000

    -My directory layout got ass-reamed by some typo.  This commit is procedural in attempt to fix it.

---
+++ trunk/storyscript/interpreter/TotalClean.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+system( "scons --clean" );
+system( "ant clean" );
+system( "make clean" );
+system( "rm bin/*" );
+system( "rm win32/Release/*" );
+system( "rm win32/Debug/*" );
+system( "rm \"win32/Release (Static)\"/*" );
+system( "rm \"win32/Debug (Static)\"/*" );
+
