b50480579fc1189b7af339b77b38674b92e1fe62
danielcj <danielcj@dc183c02-a200-0410-8664-8f6ecc32c94b>
2006-06-23 14:52:43 +0000

    Yet another procedural commit trying to fix the absolute train-wreck that is my repository.

--- trunk/storyscript/cli/ssiconsole/linux-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#This script is dumb, and should be used through a build system (ant).
-#Ant however is also dump and won't execute my command correctly.
-
-install -v storyscript /usr/bin/
-
